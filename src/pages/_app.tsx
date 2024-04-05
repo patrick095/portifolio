4f3f50f05cb2cd4b814d1599665cc922e28e8f59
Patrick Chaves <patrick095@gmail.com>
2024-04-05 00:26:17 -0300

    [FIX] - implementando analitics da vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import PageDefault from "../components/pageDefault";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <PageDefault>
             <SpeedInsights/>
+            <Analytics/>
             <Component {...pageProps} />
         </PageDefault>
     );
